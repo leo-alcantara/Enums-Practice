0cdcd30db396c7cbd6caea142209d637cc061060
leoodindealcantara <leoalcantara@icloud.com>
2021-06-23 14:35:36 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/Dice.java
@@ -0,0 +1,51 @@
+package se.lexicon;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public enum Dice {
+
+    D6(6) {
+        public double roll() {
+            Random random = new Random();
+            return random.nextInt(6) + 1;
+        }
+    },
+    D10(10) {
+        public double roll() {
+            Random random = new Random();
+            return random.nextInt(10) + 1;
+        }
+    },
+    D20(20) {
+        public double roll() {
+            Random random = new Random();
+            return random.nextInt(20) + 1;
+        }
+    },
+    D100(100) {
+        public double roll() {
+            Random random = new Random();
+            return random.nextInt(20) + 1;
+        }
+    };
+
+    private final int maxValue;
+
+
+    Dice(int value) {
+        this.maxValue = value;
+    }
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+
+    public double roll() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
+
+
+}
---
+++ src/main/java/se/lexicon/DiceApplication.java
@@ -0,0 +1,18 @@
+package se.lexicon;
+
+public class DiceApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println(Dice.D6.roll());
+        System.out.println(Dice.D10.roll());
+        System.out.println(Dice.D20.roll());
+        System.out.println(Dice.D100.roll());
+
+
+
+
+    }
+
+
+}
